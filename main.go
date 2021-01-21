dfb33d37494c01222df217f0f1d43cc2a0684285
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2021-01-21 19:15:21 +0100

    Moved all protocol stuff to seperate package
    The test still fail but this commit does not change anything about the
    code but just moves over the code.

--- main.go
+++ main.go
@@ -16,16 +16,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"time"
 
 	"github.com/LucasRouckhout/mcstat/logger"
+	"github.com/LucasRouckhout/mcstat/protocol"
 )
 
 var (
@@ -36,14 +34,6 @@ var (
 )
 
 var LOGGER logger.Logger
-
-type Status struct {
-    Online bool                 // online or offline?
-    Version string              // server version
-    Motd string                 // message of the day
-    CurrentPlayers string       // current number of players online
-    MaxPlayers string           // maximum player capacity
-}
 
 func main() {
     flag.Parse()
@@ -65,7 +55,7 @@ func main() {
 
     http.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
         LOGGER.Infof("Getting status from %s:%d\n", *address, *port)
-        status, err := GetStatus(*address, *port)
+        status, err := protocol.GetStatus(*address, *port)
 
         if err != nil {
             LOGGER.Error(err.Error())
@@ -82,84 +72,3 @@ func main() {
     LOGGER.Infof("Running mcstat on port %s\n", serverPort)
     log.Fatal(http.ListenAndServe(":" + serverPort, nil))
 }
-
-// Retrieves the status of the minecraft server at given Address and Port.
-// The initial inspiration for this code was gathered from 
-// https://github.com/ldilley/minestat/blob/master/Go/minestat/minestat.go
-// I rewrote and documented most parts to make it more idiomatic.
-//
-// This method uses the old 1.6 minecraft protocol to get a simple status response
-// from the server by sending over a set of specific bytes over a TCP socket.
-// Modern servers still respond to this protocol correctly so this will also
-// work with servers who run anything newer than 1.6. 
-// 
-// Anything older then 1.6 is not supported.
-func GetStatus(address string, port int) (Status, error) {
-    portString := fmt.Sprint(port)
-
-    // Opens a TCP connection to the given address and port.
-    // Also accepts a timeout so that we fail fast if the opening the connection takes to long 
-    LOGGER.Debugf("Attempting to open TCP connection to %s:%s\n", address, portString)
-    conn, err := net.DialTimeout("tcp", address + ":" + portString, time.Duration(5) * time.Second)
-    defer conn.Close()
-
-    if err != nil {
-        return Status{}, err
-    }
-
-    // Write the following bytes to the TCP connection:
-    // FE 01 (hex)
-    // This is the legacy protocol way of doing a Server List ping
-    // More info https://wiki.vg/Server_List_Ping#1.6
-    LOGGER.Debugf("Sending Server List Ping to %s:%d\n", address, port)
-    _, err = conn.Write([]byte("\xFE\x01"))
-
-    if err != nil {
-        return Status{}, err
-    }
-
-    // Read the raw response from our Server List Ping
-    r := make([]byte, 512)
-    _, err = conn.Read(r)
-
-    LOGGER.Debugf("Retrieved response from %s:%d\n%X\n", address, port, r)
-
-    if err != nil {
-        return Status{}, err
-    }
-
-    // Create a Status struct from this response
-    status, err := NewStatus(r)
-
-    if err != nil {
-        return Status{}, err
-    }
-
-    return status, nil
-}
-
-// Creates a Status struct from the structured
-// response retrieved from a Server List Ping of 
-// a minecraft server.
-//
-// The structure of such a response in a byte sequece
-// (Big Endian) which is structured like so: 
-// https://wiki.vg/Server_List_Ping#1.6
-func NewStatus(b []byte) (Status, error) {
-    r := bytes.Split(b, []byte("\x00\x00\x00"))
-
-    return Status {
-        Online: true,
-        Version: string(cutByteSlice(r[2])),
-        Motd: string(cutByteSlice(r[3])),
-        CurrentPlayers: string(cutByteSlice(r[4])),
-        MaxPlayers: string(cutByteSlice(r[5])),
-    }, nil
-}
-
-// Helper method which cuts out all occurences of \x00\x00
-// out of a byte slice
-func cutByteSlice(b []byte) []byte {
-    return bytes.ReplaceAll(b, []byte("\x00"), []byte(""))
-}
-
--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,6 +1,90 @@
 package protocol
 
-// This package contains the parts of the Minecraft protocol
-// which are needed for the working of mcstat
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"time"
+)
 
+// Represents the status of a Minecraft server
+type Status struct {
+    Online bool                 // online or offline?
+    Version string              // server version
+    Motd string                 // message of the day
+    CurrentPlayers string       // current number of players online
+    MaxPlayers string           // maximum player capacity
+}
 
+// Retrieves the status of the minecraft server at given Address and Port.
+// The initial inspiration for this code was gathered from
+// https://github.com/ldilley/minestat/blob/master/Go/minestat/minestat.go
+// I rewrote and documented most parts to make it more idiomatic.
+//
+// This method uses the old 1.6 minecraft protocol to get a simple status response
+// from the server by sending over a set of specific bytes over a TCP socket.
+// Modern servers still respond to this protocol correctly so this will also
+// work with servers who run anything newer than 1.6.
+//
+// Anything older then 1.6 is not supported.
+func GetStatus(address string, port int) (Status, error) {
+    conn, err := net.DialTimeout("tcp", address + ":" + fmt.Sprint(port), time.Duration(5) * time.Second)
+
+    if err != nil {
+        return Status{}, err
+    }
+
+    // defer after the return of the err otherwise 
+    //you will get nasty nullpointers
+    defer conn.Close() 
+
+    // Write the following bytes to the TCP connection:
+    // FE 01 (hex)
+    // This is the legacy protocol way of doing a Server List ping
+    // More info https://wiki.vg/Server_List_Ping#1.6
+    _, err = conn.Write([]byte("\xFE\x01"))
+
+    if err != nil {
+        return Status{}, err
+    }
+
+    buf := make([]byte, 512)
+    _, err = conn.Read(buf)
+
+    if err != nil {
+        return Status{}, err
+    }
+
+    status, err := newStatus(buf)
+
+    if err != nil {
+        return Status{}, err
+    }
+
+    return status, nil
+}
+
+// Creates a Status struct from the structured
+// response retrieved from a Server List Ping of 
+// a minecraft server.
+//
+// The structure of such a response in a byte sequece
+// (Big Endian) which is structured like so: 
+// https://wiki.vg/Server_List_Ping#1.6
+func newStatus(b []byte) (Status, error) {
+    r := bytes.Split(b, []byte("\x00\x00\x00"))
+
+    return Status {
+        Online: true,
+        Version: string(cutByteSlice(r[2])),
+        Motd: string(cutByteSlice(r[3])),
+        CurrentPlayers: string(cutByteSlice(r[4])),
+        MaxPlayers: string(cutByteSlice(r[5])),
+    }, nil
+}
+
+// Helper method which cuts out all occurences of \x00\x00
+// out of a byte slice
+func cutByteSlice(b []byte) []byte {
+    return bytes.ReplaceAll(b, []byte("\x00"), []byte(""))
+}
