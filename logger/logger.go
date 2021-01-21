e66b1231266b0a64f9d5afe6e3757627d53ac142
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2021-01-21 18:57:57 +0100

    Cleaning up the logger package.

--- logger/logger.go
+++ logger/logger.go
@@ -23,6 +23,16 @@ const (
 // 
 // You can only use one of the predefined logging levels. Which
 // are: DEBUG, INFO, ERROR
+// 
+// You can then use this Logger object to call the respective 
+// Info, Debug and Error functions which will log a log of that
+// level. Depending on your log level the log will be written to
+// stdout or not. So for example if you have set your level to 
+// ERROR then logging with the Info() function will have no effect.
+//
+// There are also formatted equivalents to all the logging functions.
+// So Info() --> Infof() will allow you to log with formatted strings
+// just like the Print and Printf functions in the standard library.
 func NewLogger(level uint) Logger {
 
     if level != DEBUG && level != INFO && level != ERROR {
@@ -36,36 +46,36 @@ func NewLogger(level uint) Logger {
     }
 }
 
-// Logs an DEBUG level log.
+// Logs a DEBUG level log.
 func (logger Logger) Debug(message string) {
     logger.Debugf("%s\n", message)
 }
 
-// Logs an DEBUG level log with formated string.
+// Logs a DEBUG level log with formated string.
 func (logger Logger) Debugf(format string, v... interface{}) {
     if logger.Level <= DEBUG {
         log.Printf("DEBUG: " + format, v...)
     }
 }
 
-// Logs an INFO level log.
+// Logs a INFO level log.
 func (logger Logger) Info(message string) {
     logger.Infof("%s\n", message)
 }
 
-// Logs an INFO level log with formated string.
+// Logs a INFO level log with formated string.
 func (logger Logger) Infof(format string, v... interface{}) {
     if logger.Level <= INFO {
         log.Printf("INFO: " + format, v...)
     }
 }
 
-// Logs an ERROR level log.
+// Logs a ERROR level log.
 func (logger Logger) Error(message string) {
     logger.Errorf("%s\n", message)
 }
 
-// Logs an ERROR level log with formated string.
+// Logs a ERROR level log with formated string.
 func (logger Logger) Errorf(format string, v... interface{}) {
     log.Printf("ERROR: " + format, v...)
 }
